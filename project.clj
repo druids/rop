d02d9887e436765feed243b992d9cf626bf8026b
Lukas Rychtecky <lukas.rychtecky@gmail.com>
2018-02-07 18:16:44 +0100

    Moved Clojure into dev dependencies and upgraded to 1.9.0

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "MIT License"
             :url "http://opensource.org/licenses/MIT"}
 
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [funcool/cats "2.1.0"]]
+  :dependencies [[funcool/cats "2.1.0"]]
 
   :profiles {:dev {:plugins [[lein-cloverage "1.0.10"]
                              [lein-kibit "0.1.6"]
-                             [jonase/eastwood "0.2.5"]]}})
+                             [jonase/eastwood "0.2.5"]]
+                   :dependencies [[org.clojure/clojure "1.9.0"]]}})
